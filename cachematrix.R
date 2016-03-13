434398aa1c3f4a58157f47c8a3acc7353063571e
caroline6927 <caroline6927@gmail.com>
2016-03-13 13:26:37 +0200

    assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##cache a matrix and check if there is a cache if the new matrix is the same as the old one. 
+##retrieve if yes and compute inverse if no.
 
-## Write a short comment describing this function
+## Set a list of functions to get and set a matrix and get and set the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInver <- function(Inver) m <<- Inver
+  getInver <- function() m
+  list(set = set, get = get,
+       setInver = setInver,
+       getInver = getInver)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInver()
+  if(!is.null(m) & identical(x,m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInver(m)
+  m
 }
